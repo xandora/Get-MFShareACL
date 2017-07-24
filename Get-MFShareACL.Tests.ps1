77424aa9a6e525c5ab1b8988af93d6e84592df86
Xandora <mfra109@gmail.com>
2017-07-24 23:33:12 +1200

    Recreate Files to start development using TDD w/ Pester.

---
+++ Get-MFShareACL.Tests.ps1
@@ -0,0 +1,9 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here${directorySeparatorChar}$sut"
+
+Describe "Get-MFShareACL" {
+    It "does something useful" {
+        $true | Should Be $false
+    }
+}
