f1610c8e5fa8ef719ec73126d6d4d4fb7ec34cc2
xandora <mfra109@gmail.com>
2019-02-08 10:01:57 +1300

    Added logic to scan parent folder as well

--- Get-MFShareACL.ps1
+++ Get-MFShareACL.ps1
@@ -1,110 +1,110 @@
 ﻿function Get-MFShareACL {
-    [CmdletBinding()]
-    Param(
-        # Target Path
-        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 0)] $Path,
-        [Parameter(Mandatory = $false)]                                                   $OutputPath = "C:\Powershell\LOGS"
-    )
+  [CmdletBinding()]
+  param(
+    # Target Path
+    [Parameter(Mandatory = $true,ValueFromPipelineByPropertyName = $true,Position = 0)] $Path,
+    [Parameter(Mandatory = $false)] $OutputPath = "C:\Powershell\LOGS"
+  )
 
-    Begin {
-    
-        $stopWatch = [System.Diagnostics.Stopwatch]::StartNew()    
-        . $PSScriptRoot\Get-LocalGroup.ps1
+  begin {
 
-        $Script:userSID = ''
-        $Script:user = ''
-        $domain = 'BAT' #Insert AD Domain name to split from usernames in here.
+    $stopWatch = [System.Diagnostics.Stopwatch]::StartNew()
+    . $PSScriptRoot\Get-LocalGroup.ps1
 
-        Write-Host "Gathering Folders from $Path..."
+    $Script:userSID = ''
+    $Script:user = ''
+    $domain = 'BAT' #Insert AD Domain name to split from usernames in here.
 
-        $date = get-date -Format yyyMMddhhmmss
-        $folders = Get-ChildItem -Path $Path -Directory -Recurse -ErrorAction SilentlyContinue -ErrorVariable err
-        $folderName = [System.IO.Path]::GetFileName($Path)
+    Write-Host "Gathering Folders from $Path..."
 
-        foreach ($errorRecord in $err) {
-            if ($errorRecord.Exception -is [System.IO.PathTooLongException]) {
-                Write-Warning "Path too long in directory '$($errorRecord.TargetObject)'."
+    $date = Get-Date -Format yyyMMddhhmmss
+    $parentFolder = Get-Item -Path $Path -ErrorAction SilentlyContinue -ErrorVariable err
+    $childFolders = Get-ChildItem -Path $Path -Directory -Recurse -ErrorAction SilentlyContinue -ErrorVariable err
+    $folderName = [System.IO.Path]::GetFileName($Path)
+
+    foreach ($errorRecord in $err) {
+      if ($errorRecord.Exception -is [System.IO.PathTooLongException]) {
+        Write-Warning "Path too long in directory '$($errorRecord.TargetObject)'."
+      }
+      else {
+        Write-Error -ErrorRecord $errorRecord
+      }
+    }
+
+    Write-Host "Gathering Groups..."
+    $groups = Get-LocalGroup -ComputerName $Path.Split("\")[2]
+
+    if (!(Test-Path $OutputPath)) {
+      New-Item -Path $OutputPath -ItemType Directory
+    }
+
+  }
+
+  process {
+
+    Write-Host "Processing Folders..."
+    function Process-Folders($folders) {
+      $access = (Get-Acl -Path $f.FullName)
+
+      foreach ($u in $access.Access) {
+
+        if ($u.IdentityReference -like "$domain") {
+          try {
+            [string]$uIdent = $u.IdentityReference
+            $displayName = Get-ADUser $uIdent.Split("\")[1] -Properties DisplayName | Select-Object DisplayName
+            $user = "$($displayName.DisplayName) ($MUDID)"
+          }
+          catch {
+            $user = "$($u.IdentityReference) (User not found)"
+          }
+
+        }
+        elseif ($u.IdentityReference -like "S-*") {
+          # Figure out a way to use the Get-LocalGroups function here to convert local group SIDs to group names.
+          foreach ($g in $groups) {
+            if ($g.SID -like $u.IdentityReference) {
+              $userSID = $g.Name
+
             }
             else {
-                Write-Error -ErrorRecord $errorRecord
+              $userSID = $u.IndentityReference
             }
+          }
+          $user = $userSID
         }
-        
-        Write-Host "Gathering Groups..."
-        $groups = Get-LocalGroup -Computername $Path.Split("\")[2]
+        else {
+          $user = $u.IdentityReference
+        }
 
-        # WRITE A TEST TO CHECK THE $OUTPUTPATH FOLDER ACTUALLY EXISTS. PROMPT FOR CREATION IF IT DOES NOT.
-        if (!(Test-Path $OutputPath)) {
-            New-Item -Path $OutputPath -ItemType Directory
+        $output = [ordered]@{
+          FullName = $f.FullName
+          Directory = $f.Name
+          User = $user
+          Access = $u.FileSystemRights
+          AccessType = $u.AccessControlType
+          Inherited = $u.IsInherited
+          InheritanceFlags = $u.InheritanceFlags
         }
+
+        New-Object psobject -Property $output | Export-Csv $OutputPath\$folderName-$date.csv -Append -NoTypeInformation
+
+      }
 
     }
 
-    Process {
-
-        Write-Host "Processing Folders..."
-        foreach ($folder in $folders) {
-            $access = (get-acl -Path $folder.FullName)
-
-            foreach ($u in $access.Access) {
-
-                if ($u.IdentityReference -like "$domain") {
-                    Try {
-                        [String]$uIdent = $u.IdentityReference
-                        $displayName = get-aduser $uIdent.split("\")[1] -Properties DisplayName | Select-Object DisplayName
-                        $user = "$($displayName.DisplayName) ($MUDID)"
-                    }
-                    Catch {
-                        $user = "$($u.IdentityReference) (User not found)"
-                    }
-
-                }
-                elseif ($u.IdentityReference -like "S-*") {
-                    # Figure out a way to use the Get-LocalGroups function here to convert local group SIDs to group names.
-                    # write-host "SID"
-                    foreach ($g in $groups) {
-                        if ($g.SID -like $u.IdentityReference) {
-                            #  Write-Host $u.IdentityReference
-                            #Write-Host $g.SID
-                            # Write-Host $g.Name
-                            $userSID = $g.Name
-
-                        }
-                        else {
-                            $userSID = $u.IndentityReference
-                        }
-                    }
-                    $user = $userSID
-                    # Write-Host "User: $user"
-                }
-                else {
-                    $user = $u.IdentityReference
-                }
-
-                $output = [ordered]@{
-                    FullName         = $folder.FullName
-                    Directory        = $folder.Name
-                    User             = $user
-                    Access           = $u.FileSystemRights
-                    AccessType       = $u.AccessControlType
-                    Inherited        = $u.IsInherited
-                    InheritanceFlags = $u.InheritanceFlags
-                }
-
-                New-Object psobject -property $output | Export-Csv $OutputPath\$folderName-$date.csv -Append -NoTypeInformation
-                #$results = @(New-Object psobject -property $output)
-                #Write-Output $errorRecord | Export-Csv $OutputPath\$folderName-$date-Error.csv -Append -NoTypeInformation
-
-            }
-
-        }
-
+    foreach ($f in $parentFolder) {
+      Process-Folders($f)
     }
 
-    End {
-        $stopWatch.Stop()
-        Write-Output "Script completed in: $($stopWatch.Elapsed.Hours) : $($stopWatch.Elapsed.Minutes) : $($stopWatch.Elapsed.Seconds)"
+    foreach ($f in $childFolders) {
+      Process-Folders($f)
     }
+  }
+
+  end {
+    $stopWatch.Stop()
+    Write-Output "Script completed in: $($stopWatch.Elapsed.Hours) : $($stopWatch.Elapsed.Minutes) : $($stopWatch.Elapsed.Seconds)"
+  }
 
 }
 
