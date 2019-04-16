e322bf5cbe9ea47c48a99bc1b036c3be8bdc2a23
xandora <mfra109@gmail.com>
2019-04-16 13:47:50 +1200

    Delete Get-MFShareACL.Tests.ps1
    File no longer needed.

--- Get-MFShareACL.Tests.ps1
+++
@@ -1,9 +0,0 @@
-﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
-. "$here${directorySeparatorChar}$sut"
-
-Describe "Get-MFShareACL" {
-    It "gets share ACLs" {
-        Get-MFShareACL | Should Be ""
-    }
-}
